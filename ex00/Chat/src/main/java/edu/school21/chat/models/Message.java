2f7795b33955df61b064d84da6a9f0b0d2ac693e
wabathur <safbul@gmail.com>
2022-10-30 15:23:27 +0300

    ex00 start

---
+++ ex00/Chat/src/main/java/edu/school21/chat/models/Message.java
@@ -0,0 +1,78 @@
+package edu.school21.chat.models;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class Message {
+    private Long id;
+    private User author;
+    private Chatroom room;
+    private String text;
+    private LocalDateTime dateTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Chatroom getRoom() {
+        return room;
+    }
+
+    public void setRoom(Chatroom room) {
+        this.room = room;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Message message = (Message) o;
+        return Objects.equals(id, message.id) && Objects.equals(author, message.author)
+                && Objects.equals(room, message.room) && Objects.equals(text, message.text)
+                && Objects.equals(dateTime, message.dateTime);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, author, room, text, dateTime);
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "id=" + id +
+                ", author=" + author +
+                ", room=" + room +
+                ", text='" + text + '\'' +
+                ", dateTime=" + dateTime +
+                '}';
+    }
+}
